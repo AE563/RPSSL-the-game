392596e99a16b9064ef1db53df5b27581745863e
Ivan Zhilnikov <feliksesae@gmail.com>
2023-03-09 20:24:05 +0400

    changed import. improved readability

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import player_input
-import computer_options
-import determine_winner
-import announce
+from player_input import player_choice
+from computer_options import computer_choice
+from determine_winner import who_won
+from announce import announce_winner
 
-player_fighter = player_input.player_choice()
 
-computer_fighter = computer_options.computer_choice()
+player_fighter = player_choice()
 
-winner = determine_winner.who_won(player_fighter, computer_fighter)
+computer_fighter = computer_choice()
 
-announce.announce_winner(player_fighter, computer_fighter, winner)
+winner = who_won(player_fighter, computer_fighter)
+
+announce_winner(player_fighter, computer_fighter, winner)

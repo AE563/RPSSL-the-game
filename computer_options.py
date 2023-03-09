794b172de10928082050a5e3f228120190ee0d72
Ivan Zhilnikov <feliksesae@gmail.com>
2023-03-09 20:36:42 +0400

    balance changes

--- computer_options.py
+++ computer_options.py
@@ -2,14 +2,14 @@ import random
 
 
 def computer_choice():
-    y = random.randint(1, 5)
-    if y == 5:
+    y = random.randint(1, 100)
+    if y > 80:
         return 'Rock'
-    elif y == 4:
+    elif y > 60:
         return 'Paper'
-    elif y == 3:
+    elif y > 40:
         return 'Scissors'
-    elif y == 2:
+    elif y > 20:
         return 'Spock'
     else:
         return 'Lizard'
